916041386ebfd16e74ba46c9fd87a2fbc6719958
Sai Charan <bharanisaiharsha@gmail.com>
2025-11-12 13:27:32 -0600

    Added name

--- src/main/java/com/Apple/Main.java
+++ src/main/java/com/Apple/Main.java
@@ -1,17 +1,11 @@
 package com.Apple;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class SumofTwoNumbers {
+    public static void main(string[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        system.out.println("num1 + " + " + num2 + " = " + sum);
+                Sai Charan
     }
 }
